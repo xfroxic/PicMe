e70a8b0fc7d566e63ef4e88f7cf37067807737da
xfroxic <fgoshko@gmail.com>
2022-01-05 17:34:00 -0600

    Fixed Pin schema

--- picme_backend/schemas/pin.js
+++ picme_backend/schemas/pin.js
@@ -32,6 +32,11 @@ export default {
             }
         },
         {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string',
+        },
+        {
             name: 'postedBy',
             title: 'PostedBy',
             type: 'postedBy',
